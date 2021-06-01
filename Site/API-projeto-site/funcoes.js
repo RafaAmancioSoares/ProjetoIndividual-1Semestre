d54ce8269c0df0094028ed52d8ccd89219f7c05b
Jennifer Silva <jennifer.silva@bandtec.com.br>
2021-06-01 18:57:36 -0300

    atualização do header e footer

--- Site/API-projeto-site/funcoes.js
+++ Site/API-projeto-site/funcoes.js
@@ -1,14 +1,14 @@
-let login_usuario;
+let login_usuario = req.params.nomeUsuario;;
 let nome_usuario;
 
 function redirecionar_login() {
-    window.location.href = '/Home/index.html';
+    window.location.href = 'index.html';
 }
 
 
 function verificar_autenticacao() {
     login_usuario = sessionStorage.login_usuario_meuapp;
-    nome_usuario = sessionStorage.nome_usuario_meuapp;
+    nome_usuario = sessionStorage.id_usuario_meuapp;
     
     if (login_usuario == undefined)  {
         redirecionar_login();
